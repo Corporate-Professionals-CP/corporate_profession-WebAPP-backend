bad522147424fc69340ddca3817e4eb4c65aaca4
Timothy Odebunmi <dev.brymes@gmail.com>
2025-05-08 19:39:21 +0100

    [Fix]: Fix EntryPoint command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running Server"
 #alembic upgrade head
-exec gunicorn --bind 0.0.0.0:$PORT main:app --timeout 0 -w 3 -k uvicorn.workers.UvicornWorker
+exec gunicorn --bind 0.0.0.0:$PORT app.main:app --timeout 0 -w 3 -k uvicorn.workers.UvicornWorker
