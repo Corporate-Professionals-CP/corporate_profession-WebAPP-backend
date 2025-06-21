83218b8eeea68964df68817e797d43150668af94
Timothy Odebunmi <dev.brymes@gmail.com>
2025-06-21 16:12:20 +0100

    [Fix]: Deployment to Contabo

--- app/core/config.py
+++ app/core/config.py
@@ -101,6 +101,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         case_sensitive = True
         env_file_encoding = "utf-8"
+        extra = "ignore"  
 
 
 settings = Settings()
